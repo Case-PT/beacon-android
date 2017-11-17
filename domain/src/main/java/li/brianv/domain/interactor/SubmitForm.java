c22cae494bba2ff86715cc05d254496b298b93ec
jay-5 <raj69@case.edu>
2017-11-17 18:33:16 -0500

    Jay-hometown

--- domain/src/main/java/li/brianv/domain/interactor/SubmitForm.java
+++ domain/src/main/java/li/brianv/domain/interactor/SubmitForm.java
@@ -15,7 +15,7 @@ public class SubmitForm extends UseCase<Object, SubmitForm.Params> {
     private Report report;
 
     @Inject
-    SubmitForm(FormRepository formRepository, ThreadExecutor threadExecutor, PostExecutionThread postExecutionThread) {
+    SubmitForm(FormRepository formRepository, ThreadExecutor threadExecutor, PostExecutionThread postExecutionThread, Report report) {
         super(threadExecutor, postExecutionThread);
         this.formRepository = formRepository;
         this.report = report;
